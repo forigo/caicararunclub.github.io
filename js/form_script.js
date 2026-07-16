7bf01d616fab3e74feafa5665821ce12f709f408
Felipe Forigo <felipe_forigo@hotmail.com>
2026-07-16 01:30:42 -0300

    Update form_script.js

--- js/form_script.js
+++ js/form_script.js
@@ -68,20 +68,15 @@ function aplicarMascaraTelefone(valor) {
 
 //Form
 
-const script_do_google = 'https://script.google.com/macros/s/AKfycbxKU9mEwPyk_Ckp39e5J4b7x_bWiIlaaGHmbD1MtuPl7J6gIRuGFrOk29rsmHVkx_Bl/exec';
-const dados_do_formulario = document.forms['formulario'];
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxeFPLslF-rfT0P1KlrW7bGDM4FTFa6BObf_Po-TdCFCQOoRtqeCTlQmFAkmMt1VqaI/exec'
+const form = document.forms['formulario']
 
-dados_do_formulario.addEventListener('submit', function (e) {
-    e.preventDefault();
-
-    fetch(script_do_google, { method: 'POST', body: new FormData(dados_do_formulario) })
-        .then(response => {
-            // Se os dados forem gravados corretamente, será enviada uma mensagem de sucesso
-            alert('Dados enviados com sucesso!', response);
-            dados_do_formulario.reset(); 
-        })
-        .catch(error => {
-            // Se houver erro no envio, a menssagem abaixo será exibida
-            console.error('Erro no envio dos dados!', error);
-        });
-});
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  if (window.confirm('Entre no nosso grupo do whatsapp para confirmar seu cadastro.'))
+  { window.open('http://www.google.com', '_blank');};
+  togglePopup()
+  .catch(error => console.error('Erro no envio dos dados!', error.message))
+  form.reset()
+})
